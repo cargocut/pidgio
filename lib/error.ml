cae1ae27af38a784e0b0d83f06ed7933eb43415a
xvw <xavier.vdw@gmail.com>
2026-08-28 15:54:52 +0200

    Start drafting Errors

---
+++ lib/error.ml
@@ -0,0 +1,127 @@
+(* Copyright (c) 2026, Cargocut and the Pidgio developers.
+   All rights reserved.
+
+   SPDX-License-Identifier: BSD-3-Clause *)
+
+type t =
+  | Parse_error of { body : string }
+  | Invalid_request of
+      { body : string
+      ; error : Pidgin.Check.value_error
+      }
+  | Method_not_found of
+      { body : string
+      ; id : int option
+      ; meth : string
+      }
+  | Invalid_params of
+      { body : string
+      ; id : int option
+      ; error : Pidgin.Check.value_error
+      }
+  | Internal_error of
+      { body : string
+      ; id : int option
+      ; message : string
+      }
+  | Custom_error of
+      { body : string
+      ; id : int option
+      ; code : int
+      ; message : string option
+      }
+
+let parse_error ~body = Parse_error { body }
+let invalid_request ~body error = Invalid_request { body; error }
+let method_not_found ?id ~body meth = Method_not_found { body; id; meth }
+let invalid_params ?id ~body error = Invalid_params { id; body; error }
+let internal_error ?id ~body message = Internal_error { id; body; message }
+
+let custom_error ?id ~body ~code ?message () =
+  let code = 32000 + code in
+  Custom_error { id; body; code; message }
+;;
+
+let mk_code code = 0 - Int.abs code
+
+let mk ?id ?data ~body ~code message =
+  let open Pidgin.Repr in
+  record
+    [ "jsonrpc", string "2.0"
+    ; "id", option int id
+    ; ( "error"
+      , record
+          [ "code", int (mk_code code)
+          ; "message", string message
+          ; "body", string body
+          ; "data", option Fun.id data
+          ] )
+    ]
+;;
+
+let rec mk_pidgin_error err =
+  let open Pidgin.Repr in
+  match err with
+  | Pidgin.Check.Unexpected_kind { expected; given; value } ->
+    record
+      [ "message", string "Unexpected kind"
+      ; "expected", string (Pidgin.Kind.to_string expected)
+      ; "given", string (Pidgin.Kind.to_string given)
+      ; "value", value
+      ]
+  | Invalid_list { errors; value } ->
+    record
+      [ "message", string "Invalid list"
+      ; ( "errors"
+        , list_of
+            (fun (i, err) ->
+               record [ "at", int i; "error", mk_pidgin_error err ])
+            (Nel.to_list errors) )
+      ; "value", value
+      ]
+  | Invalid_record { errors; value } ->
+    record
+      [ "message", string "Invalid list"
+      ; "errors", list_of mk_pidgin_record_error (Nel.to_list errors)
+      ; "value", value
+      ]
+  | Unexpected_value { value; message } ->
+    record [ "message", string message; "value", option Fun.id value ]
+
+and mk_pidgin_record_error (err : Pidgin.Check.record_error) =
+  let open Pidgin.Repr in
+  match err with
+  | Pidgin.Check.Invalid_field { field = field :: fields; error } ->
+    record
+      [ "field", string field
+      ; "message", string "Invalid field"
+      ; "error", mk_pidgin_error error
+      ; "aliases", list_of string fields
+      ]
+  | Missing_field (field :: fields) ->
+    record
+      [ "field", string field
+      ; "message", string "Missing field"
+      ; "aliases", list_of string fields
+      ]
+  | Invalid_subrecord err ->
+    record
+      [ "message", string "Invalid subrecord"; "error", mk_pidgin_error err ]
+;;
+
+let to_pidgin = function
+  | Parse_error { body } -> mk ~body ~code:32700 "Parse error"
+  | Invalid_request { body; error } ->
+    let data = mk_pidgin_error error in
+    mk ~body ~code:32600 ~data "Invalid request"
+  | Method_not_found { body; id; meth } ->
+    let data = Pidgin.Repr.string meth in
+    mk ~body ~code:32601 ~data ?id "Method not found"
+  | Invalid_params { body; id; error } ->
+    let data = mk_pidgin_error error in
+    mk ?id ~body ~code:32602 ~data "Invalid params"
+  | Internal_error { body; id; message } -> mk ?id ~body ~code:32603 message
+  | Custom_error { body; id; code; message } ->
+    let data = Pidgin.Repr.(option string message) in
+    mk ?id ~data ~body ~code "Server error"
+;;
---
+++ lib/error.mli
@@ -0,0 +1,60 @@
+(* Copyright (c) 2026, Cargocut and the Pidgio developers.
+   All rights reserved.
+
+   SPDX-License-Identifier: BSD-3-Clause *)
+
+(** Describes the errors that can be propagated by the server (and
+    provides their Pidgin decoders). *)
+
+(** {1 Types} *)
+
+(** All errors that may occur on a Pidgio server. *)
+type t
+
+(** {1 Building errors}
+
+    There are a number of parameters that are common to the process of
+    generating errors:
+
+    - [id] the ID (int), which is optional and provided by the client.
+    - [body] The request sent as a string. The entire request as a
+      string. *)
+
+(** [parse_error ~body] when the [body] request could not be parsed. *)
+val parse_error : body:string -> t
+
+(** [invalid_request ~body validation_error] when the incoming request
+    can't be validated. *)
+val invalid_request : body:string -> Pidgin.Check.value_error -> t
+
+(** [method_not_found ?id ~body meth] when then given [meth] of the
+    request [id] is not available. *)
+val method_not_found : ?id:int -> body:string -> string -> t
+
+(** [invalid_params ?id ~body validation_error] when the incoming
+    request of [id] can't validate params. *)
+val invalid_params : ?id:int -> body:string -> Pidgin.Check.value_error -> t
+
+(** [internal_error ?id ~body message] when an internal error for the
+    request of id [id] it is raised with the given [message]. *)
+val internal_error : ?id:int -> body:string -> string -> t
+
+(** [custom_error ?id ~body ~code ?message ()] when an error occurs on
+    the server side. It is used of user-defined errors. [code] should
+    start at [0] (the function did the offset addition). *)
+val custom_error
+  :  ?id:int
+  -> body:string
+  -> code:int
+  -> ?message:string
+  -> unit
+  -> t
+
+(** {1 Serialization}
+
+    From the server's perspective, errors must be serialized only
+    because they are consumed by another client. *)
+
+(** [to_pidgin err] serialize the given [err] into a Pidgin
+    representation. *)
+val to_pidgin : t Pidgin.Repr.conv
